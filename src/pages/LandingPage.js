d0c3febb35a17352a4a4795600383b73076b5beb
scantarius <goldyy44@gmail.com>
2024-10-21 22:43:02 +0200

    landingPage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,23 +5,29 @@ import aboutBackground from "../pictures/aboutBackground.png";
 function LandingPage() {
   return (
     <>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
-      <h1>aaaa</h1>
+      <div id="landingPage">
+        <div id="landingPageWrapper">
+          <img
+            className="aboutBackground"
+            src={aboutBackground}
+            alt={aboutBackground}
+          />
+          <div className="textBox">
+            <h1 className="title">Ne traži izgovore.</h1>
+            <div class="animation">
+              <h1 className="item">jedan</h1>
+              <h1 className="item">dva</h1>
+              <h1 className="item">tri</h1>
+            </div>
+            <button>VIŠE O NAMA</button>
+          </div>
+        </div>
+      </div>
+      <div id="cards">
+        <h1>test test test</h1>
+        <h1>test test</h1>
+        <h1>test test test</h1>
+      </div>
     </>
   );
 }
